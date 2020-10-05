88c5ea5f4cae3b896539eb01cacdb666bf8b1841
Nathanael <nathanael.meshesha@yahoo.com>
2020-10-04 23:35:18 -0500

    created a show route and index now lists all product names

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,8 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-
+  def show
+    #we can use the params to find the id for a specific movie
+    @product = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   #Update the routes such that the Products controller's index action is trigger from a GET request to /products
   #(Load and display all products)
   get "/products", to: "products#index", as: "products" #products_path
+
+  get "products/:id", to: "products#show", as: "product" #product_path
 end
