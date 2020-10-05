1debd0cd90a35e57426129d1f31ad04e6f6f16fa
Nathanael <nathanael.meshesha@yahoo.com>
2020-10-05 00:08:51 -0500

    updated the seed script to pull from products.csv and create our records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-676.times{
-  new_product = Product.create(
-                              :title => Faker::Commerce.product_name,
-                              :price => Faker::Commerce.price,
-                              :stock_quantity => Faker::Number.number(digits: 3)
-                             )
-}
+require "csv"
+
+Product.destroy_all
+
+Category.destroy_all
+
+filename = Rails.root.join("db/products.csv")
+
+puts "Loading Movies from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p["category"])
+
+  if category && category.valid?
+    product = category.products.create(
+      title: p["name"],
+      description: p["description"],
+      price: p["price"],
+      stock_quantity: p["stock quantity"]
+    )
+  end
+end
